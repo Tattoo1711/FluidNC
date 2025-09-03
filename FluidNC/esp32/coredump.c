3b30d39afc43aabc8c3227c2d63d7a8ba5cb36f1
Mitch Bradley <wmb@firmworks.com>
2025-09-03 11:31:51 -1000

    Fixed coredump stub-out

--- FluidNC/esp32/coredump.c
+++ FluidNC/esp32/coredump.c
@@ -1,4 +1,3 @@
-#if 0
 // Copyright 2022 Mitch Bradley
 // Use of this source code is governed by a GPLv3 license that can be found in the LICENSE file.
 //
@@ -7,14 +6,28 @@
 // We don't want to waste space for such a partition, and the Arduino Framework
 // enables coredumps.  We override that by stubbing out these routines.
 
-#    include <stddef.h>
-#    include "esp_err.h"
-#    include "esp_private/panic_internal.h"
-#    include "esp_core_dump_summary_port.h"
+#include <stddef.h>
+#include "esp_err.h"
+#include "esp_private/panic_internal.h"
+// #include "esp_core_dump_summary_port.h"
+#include "esp_core_dump.h"
 
-#    ifdef __cplusplus
+#ifdef __cplusplus
 extern "C" {
-#    endif
+#endif
+
+// cppcheck-suppress unusedFunction
+esp_err_t esp_core_dump_get_summary(esp_core_dump_summary_t* summary) {
+    return ESP_ERR_NOT_FOUND;
+}
+
+// cppcheck-suppress unusedFunction
+void esp_core_dump_write(panic_info_t* info) {}
+
+// cppcheck-suppress unusedFunction
+esp_err_t esp_core_dump_get_panic_reason(char* reason_buffer, size_t buffer_size) {
+    return ESP_ERR_NOT_FOUND;
+}
 
 // cppcheck-suppress unusedFunction
 void esp_core_dump_init(void) {}
@@ -38,7 +51,6 @@ esp_err_t esp_core_dump_image_erase(void) {
     return ESP_OK;
 }
 
-#    ifdef __cplusplus
+#ifdef __cplusplus
 }
-#    endif
 #endif
